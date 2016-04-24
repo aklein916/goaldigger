0056ae75a4b14d10135d7fbb43e2268fc623edb9
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-04-24 14:50:44 -0400

    migrated category and goal tables

--- db/migrate/20160424183843_create_categories.rb
+++ db/migrate/20160424183843_create_categories.rb
@@ -1,6 +1,12 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
+      t.string :financial
+      t.string :health
+      t.string :career
+      t.string :personal
+      t.string :fun 
+
     end
   end
 end
