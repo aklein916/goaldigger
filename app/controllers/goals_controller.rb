2781a7241c45a17bafec6cfd4903efcb0a1dd8eb
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-04-25 15:49:00 -0400

    added nested routes and helpers for goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,19 +1,53 @@
 class GoalsController < ApplicationController
+
+  #index
   def index
-    @goals = Goal.all
+    @profile = Profile.find(params[:profile_id])
+    @goals = @profile.goals.all
   end
 
+  #show
   def show
+    @profile = Profile.find(params[:profile_id])
     @goal = Goal.find(params[:id])
   end
+
+  #new
   def new
+    @profile = Profile.find(params[:profile_id])
     @goal = Goal.new
   end
 
+  #create
   def create
-    @goal = Goal.new(goal_params)
-    @goal.save!
-    redirect_to @goal
+    @profile = Profile.find(params[:profile_id])
+    @goal = @profule.goals.create(goal_params)
+    # @goal.save!
+    redirect_to profile_goals_path(@profile, @goal)
+  end
+
+  #edit
+  def edit
+    @profile = Profile.find(params[:profile_id])
+    @goal = Goal.find(params[:id])
+  end
+
+  #update
+  def update
+    @profile = Profile.find(params[:profile_id])
+    @goal = Goal.find(params[:id])
+    @goal.update(goal_params)
+
+    redirect_to profile_goals_path(@profile, @goal)
+  end
+
+  #destroy
+  def destroy
+    @profile = Profile.find(params[:profile_id])
+    @goal = Goal.find(params[:id])
+    @goal.destroy
+
+    redirect_to profile_goals_path( @profile )
   end
 
   private
