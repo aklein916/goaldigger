12b46517966e5b88741394add7195e04bcf8b65c
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-04-25 15:37:29 -0400

    adding in nested routes for profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,20 +1,49 @@
 class ProfilesController < ApplicationController
+  #index
   def index
     @profiles = Profile.all
   end
 
+#show
   def show
-    @profile = profile.find(params[:id])
+    @profile = Profile.find(params[:id])
   end
 
+#new
   def new
     @profile = Profile.new
   end
 
+#create
   def create
-    @profile = Profile.new(profile_params)
+    profile.create(profile_params.merge(user: current_user))
+    @profile = Profile.create!(profile_params)
     @profile.save!
-    redirect_to @profile
+    redirect_to profile_path(@profile)
+  end
+  # edit
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+
+  # update
+  def update
+    @profile = Profile.find(params[:id])
+    @profile.update(profile_params)
+
+    redirect_to profile_path(@profile)
+  end
+
+  #delete
+  def destroy
+    @profile = Profile.find(params[:id])
+    if @profile.user == current_user
+      @profile.destroy
+    else
+      flash[:alert] = "Only the owner of the profile can delete"
+    end
+    redirect_to profiles_path
   end
 
   private
