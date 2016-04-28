0ce7c527fa3a2e3b9ccbf60b977c1862f06bf767
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-04-28 13:34:40 -0400

    added migration for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root "goals#home"
   resources :goals
   resources :categories
   end
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
